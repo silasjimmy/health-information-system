f54a4109efb6a3ea0413cef5087943d4974a4baf
Silas Jimmy <jimmysilas17@gmail.com>
2025-04-24 21:40:21 +0300

    refactor: create clients view layout

--- frontend/application-src/src/views/ClientsView.vue
+++ frontend/application-src/src/views/ClientsView.vue
@@ -1,7 +1,140 @@
 <template>
-    <h1>Clients</h1>
+  <div class="actions-wrapper">
+    <a-flex justify="space-between">
+      <a-input-search
+      enter-button
+        style="width: 350px"
+        placeholder="Search client's name..."
+        @search="searchClient"
+      />
+
+      <a-button type="primary">
+        <PlusOutlined />
+
+        <span>Add New Client</span>
+      </a-button>
+    </a-flex>
+  </div>
+
+  <a-table :columns="columns" :data-source="data">
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.key === 'name'">
+        <span> {{ record.firstName }} {{ record.lastName }} </span>
+      </template>
+
+      <template v-else-if="column.key === 'programs'">
+        <span>
+          <a-tag v-for="program in record.programs" :key="program">
+            {{ program.label }}
+          </a-tag>
+        </span>
+      </template>
+
+      <template v-else-if="column.key === 'action'">
+        <div>
+          <a-tooltip placement="top" title="View client">
+            <a-button shape="circle" type="text" size="large">
+              <EyeOutlined />
+            </a-button>
+          </a-tooltip>
+
+          <a-divider type="vertical" />
+
+          <a-tooltip placement="top" title="Edit client details">
+            <a-button shape="circle" type="text" size="large">
+              <EditOutlined />
+            </a-button>
+          </a-tooltip>
+
+          <a-divider type="vertical" />
+
+          <a-tooltip placement="top" title="Add program">
+            <a-button shape="circle" type="text" size="large">
+              <PlusCircleOutlined />
+            </a-button>
+          </a-tooltip>
+        </div>
+      </template>
+    </template>
+  </a-table>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  EyeOutlined,
+  PlusCircleOutlined,
+  EditOutlined,
+  PlusOutlined,
+} from "@ant-design/icons-vue";
 
-<style scoped></style>
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Programs",
+    key: "programs",
+    dataIndex: "programs",
+  },
+  {
+    title: "Action",
+    key: "action",
+  },
+];
+
+const data = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Brown",
+    programs: [
+      {
+        id: 1,
+        label: "Malaria",
+        value: "malaria",
+      },
+      {
+        id: 2,
+        label: "TB",
+        value: "tb",
+      },
+    ],
+  },
+  {
+    id: 2,
+    firstName: "Jim",
+    lastName: "Green",
+    programs: [
+      {
+        id: 1,
+        label: "Malaria",
+        value: "malaria",
+      },
+    ],
+  },
+  {
+    id: 3,
+    firstName: "Joe",
+    lastName: "Black",
+    programs: [
+      {
+        id: 3,
+        label: "HIV",
+        value: "hiv",
+      },
+    ],
+  },
+];
+
+function searchClient(name: string) {
+  console.log("client name:", name);
+}
+</script>
+
+<style scoped>
+.actions-wrapper {
+    margin-bottom: 20px;
+}
+</style>
